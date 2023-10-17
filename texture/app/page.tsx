2c92918fb8d029190b47e221cb17ac0cf77128de
Bora <borabalos9@gmail.com>
2023-10-18 02:06:49 +1100

    Form schema to handle validation with zod

--- texture/app/page.tsx
+++ texture/app/page.tsx
@@ -1,12 +1,27 @@
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 
 import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 export default function Home() {
-	const form = useForm({
+
+	const formSchema = z.object({
+		title: z
+			.string()
+			.min(5, { message: "Oops... the title isn't long enough 😅" })
+			.max(100, { message: "Oops... the title is too long 😭" }),
+		price: z.number().min(5, { message: "Oops... the price is too low 🤓" }),
+		description: z
+			.string()
+			.min(5, { message: "Oops... the description isn't long enough 😅" })
+			.max(100, { message: "Oops... the description is too long 😭" })
+			.trim(),
+	})
+	const form = useForm<z.infer<typeof formSchema>>({
 		mode: 'onChange', // tracking everytime there is a change
 		defaultValues: {
 			title: "",
+			price: 29.99,
 			description: "",
 		}
 	});
